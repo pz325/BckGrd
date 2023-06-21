57d7d2695637eff606978a5a1b09012fd866fc8a
Ping Zou <sg71.cherub@gmail.com>
2023-06-21 16:59:07 +0800

    Initial Commit

---
+++ Bckgrd/BckgrdApp.swift
@@ -0,0 +1,17 @@
+//
+//  BckgrdApp.swift
+//  Bckgrd
+//
+//  Created by Ping Zou on 21/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct BckgrdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bckgrd/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bckgrd
+//
+//  Created by Ping Zou on 21/06/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
