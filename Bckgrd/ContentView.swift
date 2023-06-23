7515a7c5d7c4943619ced5f190a66a30a9bae3e5
Ping Zou <sg71.cherub@gmail.com>
2023-06-23 16:30:16 +0800

    Button to 1) send notification 2) start a timer, which sends notification after timeout

--- Bckgrd/ContentView.swift
+++ Bckgrd/ContentView.swift
@@ -7,8 +7,11 @@
 
 import SwiftUI
 
+import UserNotifications
+
 struct ContentView: View {
     @State private var imageFilePath: String = "/Users/menrfa/Downloads/FzFDzP8XsA4kNAQ.jpeg"
+    @State private var isTimerRunning = false
     
     var body: some View {
         VStack(alignment: .leading) {
@@ -20,6 +23,15 @@ struct ContentView: View {
                 "Update",
                 action: {self.setBackground(imageFilePath: imageFilePath)}
             )
+            
+            Button("Send Notification") {
+                requestNotificationPermission()
+                scheduleNotification()
+            }
+            
+            Button("Start Timer") {
+                startTimer()
+            }
         }
         .padding()
     }
@@ -33,6 +45,65 @@ struct ContentView: View {
             }
         } catch {
             print(error)
+        }
+    }
+    
+    func requestNotificationPermission() {
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
+            if let error = error {
+                print("Error requesting notification permission: \(error.localizedDescription)")
+            } else {
+                print("Notification permission granted: \(granted)")
+            }
+        }
+    }
+    
+    func scheduleNotification() {
+        let content = UNMutableNotificationContent()
+        content.title = "New Message"
+        content.body = "You have received a new message."
+        content.sound = UNNotificationSound.default
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+        
+        UNUserNotificationCenter.current().add(request) { error in
+            if let error = error {
+                print("Error scheduling notification: \(error.localizedDescription)")
+            } else {
+                print("Notification scheduled successfully.")
+            }
+        }
+    }
+    
+    
+    func startTimer() {
+        isTimerRunning = true
+        
+        DispatchQueue.global(qos: .background).async {
+            sleep(10) // Simulating a 10-second timeout
+            
+            DispatchQueue.main.async {
+                sendNotification()
+                isTimerRunning = false
+            }
+        }
+    }
+    
+    func sendNotification() {
+        let center = UNUserNotificationCenter.current()
+        
+        let content = UNMutableNotificationContent()
+        content.title = "Timer Finished"
+        content.body = "The timer you started has finished."
+        content.sound = UNNotificationSound.default
+        
+        let request = UNNotificationRequest(identifier: "timerNotification", content: content, trigger: nil)
+        
+        center.add(request) { error in
+            if let error = error {
+                print(error.localizedDescription)
+            }
         }
     }
 }
